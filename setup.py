c7abb87a31ca6f054760fd95561cd1e347833777
José Padilla <jpadilla@webapplicate.com>
2013-03-25 17:13:09 -0400

    Remove Django from requirements

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 from setuptools import setup, find_packages
- 
+
 setup(
     name='logicaldelete',
-    version = '1.0',
+    version='1.0',
     author='Patrick Altman',
     author_email='paltman@gmail.com',
     url='http://github.com/paltman/django-logicaldelete',
     description="""django-logicaldelete is a base model that provides some extras for your models.""",
     packages=find_packages(),
-    namespace_packages = [],
-    include_package_data = True,
+    namespace_packages=[],
+    include_package_data=True,
     zip_safe=False,
-    license='MIT',
-    install_requires=[
-        'django',
-        ]
+    license='MIT'
 )
